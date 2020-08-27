d389de844793b2f10edc251f5599cbbdfb4777a7
k37x <36374985+k37x@users.noreply.github.com>
2020-08-27 13:10:38 -0400

    Cleaning

--- src/me/k37/healthy/MyPotion.java
+++ src/me/k37/healthy/MyPotion.java
@@ -29,24 +29,24 @@ public class MyPotion implements CommandExecutor{
 			if(player.hasPermission("healthy.mypotion") == true) {
 				try {
 
-				if (args.length == 3) {
-					if(isNum(args[1]) && (isNum(args[2])) ) {
+					if (args.length == 3) {
+						if(isNum(args[1]) && (isNum(args[2])) ) {
 					
-					String type = (args[0]);
-					int duration = Integer.parseInt(args[1]);
-					int amplifier = Integer.parseInt(args[2]);
+						String type = (args[0]);
+						int duration = Integer.parseInt(args[1]);
+						int amplifier = Integer.parseInt(args[2]);
 					
 					
 					
-					player.addPotionEffect(new PotionEffect(PotionEffectType.getByName(type), duration, amplifier));
-					return true;
+						player.addPotionEffect(new PotionEffect(PotionEffectType.getByName(type), duration, amplifier));
+						return true;
+						}
+					
+					
+					} else {
+						player.sendMessage(ChatColor.RED + "Do /mp <type> [duration] [amplifier]");
+						player.sendMessage(ChatColor.RED + "Types: ABSORPTION, BAD_OMEN, BLINDNESS, CONDUIT_POWER, CONFUSION, DAMAGE_RESISTANCE, DOLPHINS_GRACE, FAST_DIGGING, FIRE_RESISTANCE, GLOWING, HARM, HEAL, HEALTH_BOOST, HERO_OF_THE_VILLAGE, HUNGER, INCREASE_DAMAGE, INVISIBILITY, JUMP, LEVITATION, LUCK, NIGHT_VISION, POISON, REGENERATION, SATURATION, SLOW, SLOW_DIGGING, SLOW_FALLING, SPEED, UNLUCK, WATER_BREATHING, WEAKNESS ");
 					}
-					
-					
-				} else {
-					player.sendMessage(ChatColor.RED + "Do /mp <type> [duration] [amplifier]");
-					player.sendMessage(ChatColor.RED + "Types: ABSORPTION, BAD_OMEN, BLINDNESS, CONDUIT_POWER, CONFUSION, DAMAGE_RESISTANCE, DOLPHINS_GRACE, FAST_DIGGING, FIRE_RESISTANCE, GLOWING, HARM, HEAL, HEALTH_BOOST, HERO_OF_THE_VILLAGE, HUNGER, INCREASE_DAMAGE, INVISIBILITY, JUMP, LEVITATION, LUCK, NIGHT_VISION, POISON, REGENERATION, SATURATION, SLOW, SLOW_DIGGING, SLOW_FALLING, SPEED, UNLUCK, WATER_BREATHING, WEAKNESS ");
-				}
 				} catch (Exception e){
 					player.sendMessage(ChatColor.RED + "Do /mp <type> [duration] [amplifier]");
 					player.sendMessage(ChatColor.RED + "Types: ABSORPTION, BAD_OMEN, BLINDNESS, CONDUIT_POWER, CONFUSION, DAMAGE_RESISTANCE, DOLPHINS_GRACE, FAST_DIGGING, FIRE_RESISTANCE, GLOWING, HARM, HEAL, HEALTH_BOOST, HERO_OF_THE_VILLAGE, HUNGER, INCREASE_DAMAGE, INVISIBILITY, JUMP, LEVITATION, LUCK, NIGHT_VISION, POISON, REGENERATION, SATURATION, SLOW, SLOW_DIGGING, SLOW_FALLING, SPEED, UNLUCK, WATER_BREATHING, WEAKNESS ");
